96824eb0a417e5fe0314517da3ca20d75c663148
osvaldocer <osvaldocervantes@college.harvard.edu>
2020-12-14 18:31:05 -0800

    test commit.

--- Final_Project/app.R
+++ Final_Project/app.R
@@ -6,6 +6,9 @@ library(gtsummary)
 library(broom.mixed)
 library(rstanarm)
 
+# While anxiety and overwatch are being implemented, data is a remnant of my 
+# previous topic
+
 data <- read_rds("steam_data_set.rds")
 anxiety <- read_rds("anxiety.rds")
 overwatch <- read_rds("overwatch.rds")
@@ -26,13 +29,24 @@ ui <- fluidPage(theme = shinytheme("united"),
                           hours spent gaming based on the self-reported effect 
                           of a range of anxiety-related questions on work and 
                           home relations"),
+                        p("As demonstrated by this graph, when set to the 
+                          maximum participant numbers of 9995, The hours played 
+                          are highest cumulatively under the 'Not difficult at 
+                          all' and 'somewhat difficult' columns. Alternatively 
+                          participants who rated their anxiety's effects in 
+                          their life as 'Very Difficult' or 'Extremely 
+                          difficult' had a significantly smaller amount of hours
+                          played. This suggests that those who suffer from 
+                          anxiety are typically less likely to be able to play 
+                          videogames regularly, as their time potentially 
+                          becomes consumed by other aspects of their lives."),
                         sidebarLayout(
                             sidebarPanel(
                                 sliderInput("participants", 
                                             "Number of participants:", 
-                                            min = 1, 
+                                            min = 500, 
                                             max = 9995,
-                                            value = 100)
+                                            value = 5000)
                                 ),
                             mainPanel(
                                 plotOutput("distPlot1")
@@ -47,44 +61,73 @@ ui <- fluidPage(theme = shinytheme("united"),
                                         two weeks prior to the survey. The 
                                         scores reflect different levels of 
                                         anxiety in an individual, with higher 
-                                        scores being increased anxiety."), 
+                                        scores being increased anxiety. As 
+                                        presented in this scatter plot, there 
+                                        seems to be a general upward trend 
+                                        related to the GAD, or Generalized 
+                                        Anxiety Disorder. That is to say, in 
+                                        general, as the total score of one's 
+                                        GAD goes up, so too do average hours 
+                                        played. While one may think this
+                                        contradictory to the previous plot, 
+                                        which seems to establish that there are 
+                                        less hours when one self-describes their
+                                        experiences as incredibly stressful, 
+                                        this is simply a visualization of 
+                                        understanding totals versus average 
+                                        values. On average, those with 
+                                        increased stress play for more hours. 
+                                        However, in general, there are also less
+                                        people under the descriptors of 
+                                        'Extremely difficult' and 
+                                        'Very difficult.' As a result, this 
+                                        analysis can only establish that those 
+                                        with higher anxiety play for longer 
+                                        hours."),
                                       mainPanel(
                                           plotOutput("distPlot2")
                                           )
                                       )
                         ), 
-                   tabPanel("Model")
-                   )
+                   tabPanel("Model", 
+                            titlePanel("Analyzing Hours Played to Anxiety"), 
+                            p("this is my analysis of the model", 
+                              gt_output("model_table"), 
+                              titlePanel("additional model"), 
+                              p("additional analysis"))
+                            )
+               )
                ),
                tabPanel("About",
                         h1("About the data"),
-                        p("This data is from the gaming platform Steam. More 
-                          specifically, I intend to use existing Steam data to 
-                          complete my final project. Currently I am considering 
-                          some sort of comparative analysis, as several 
-                          variables are contained within the data which would 
-                          serve to create clear visuals. Additionally, I can 
-                          include data from Steam users as well to cross 
-                          reference and perhaps analyze my regions in the US, or
-                          other demographic information. As such I am still 
-                          searching for other potential data sets to include in 
-                          my final project. Further, I will take the time in the
-                          coming week to analyze and clean up my data to remove 
-                          the abnormalities presenting themselves in the current
-                          version of my graph."),
-                        br(),
-                        h2("UPDATE"), 
-                        p("As time has gone on, this project has significantly 
-                          evolved from my initial interests. While initially, I 
-                          had proposed this project as one that would 
-                          incorporate Steam data in analysis. However, upon 
-                          reflection, I have instead honed in on the 
-                          intersections of gender and psychology in various 
-                          aspects of gaming. More particularly, I intend on 
-                          investigating how behavioral characteristics impact 
-                          gaming and life outside of gaming, whether through 
-                          character selection, or other such levels of 
-                          analysis."),
+                        p("This data is from two separate studies related to 
+                          mental health and gaming in one way or another. One 
+                          study, titled ‘Character choice in online gaming,’ was
+                          performed in 2018 and focused on the videogame 
+                          Overwatch, in which several different playable 
+                          characters are available to the player, tailor-made 
+                          for specific roles in game. As these character 
+                          selections are vital to overall team composition and 
+                          cohesiveness, this study sought to map the playstyle 
+                          of individuals and find links to character selection 
+                          and psychological evaluations."),
+                        p("The Overwatch dataset can be found", 
+                          a("here", 
+                            href = "https://data.mendeley.com/datasets/f4zdfsx88z/2")),
+                        p("The other study, entitled ‘Social Context and Gaming 
+                          Motives Predict Mental Health Better than Time Played:
+                          An Exploratory Regression Analysis with Over 13,000 
+                          Video Game Players,’ conducted an extensive study of 
+                          gaming habits through the lens of various 
+                          psychological examinations. In particular, three 
+                          metrics were used in the creation of this dataset—the 
+                          Generalized Anxiety Disorder, Satisfaction with Life 
+                          Scale, and Social Phobia Inventory. Other auxiliary 
+                          metrics were also collected, including place of 
+                          residence."), 
+                        p("This dataset can be found", 
+                          a("here", 
+                            href = "https://www.kaggle.com/divyansh22/online-gaming-anxiety-data")),
                         br(),
                         h2("About Me"), 
                         br(),
@@ -93,14 +136,13 @@ ui <- fluidPage(theme = shinytheme("united"),
                           first foray into data science, and I enjoy analyzing 
                           the data behind government processes."),
                         h2("About this app"),
-                        br(), 
+                        br(),
                         p("My repo can be found at", 
                           a("https://github.com/osvaldocer/gaming_psychology", 
                             href = "https://github.com/osvaldocer/gaming_psychology")
                           )
                         )
-               )
-    )
+               ))
 
 
 server <- function(input, output) {
@@ -126,6 +168,36 @@ server <- function(input, output) {
                  x = "Score on GAD Scale", 
                  y = "Average Hours Played") +
             theme_bw()
+    })
+    
+    output$model_table1 <- render_gt({
+        fit_model <- stan_glm(data = anxiety, 
+                              hours ~ gender + gade + gad_t, 
+                              family = gaussian, 
+                              refresh = 0)
+        
+        tbl_regression(fit_model) %>%
+            as_gt() %>%
+            tab_header(title = "Regression of Hours Spent Gaming", 
+                       subtitle = "The Link Between Hours Played, Gender, and 
+                       Anxiety") %>%
+            tab_source_note("Source: Study Published by Marian Sauter and Dejan 
+                            Draschkow")
+    })
+    
+    output$model_table2 <- render_gt({
+        fit_model <- stan_glm(data = anxiety, 
+                              hours ~ gender + gade + gad_t, 
+                              family = gaussian, 
+                              refresh = 0)
+        
+        tbl_regression(fit_model) %>%
+            as_gt() %>%
+            tab_header(title = "Regression of Hours Spent Gaming", 
+                       subtitle = "The Link Between Hours Played, Gender, and 
+                       Anxiety") %>%
+            tab_source_note("Source: Study Published by Marian Sauter and Dejan 
+                            Draschkow")
     })
 }
 
